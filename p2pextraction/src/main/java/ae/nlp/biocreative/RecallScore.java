0b1037b6761195a1a5308506746ac257d3d3d8bf
elangovana <aparnaelangovan@gmail.com>
2017-08-05 21:56:24 +1000

    Add recall scoring

---
+++ p2pextraction/src/main/java/ae/nlp/biocreative/RecallScore.java
@@ -0,0 +1,95 @@
+package ae.nlp.biocreative;
+
+import com.pengyifan.bioc.BioCCollection;
+import com.pengyifan.bioc.BioCDocument;
+import com.pengyifan.bioc.BioCRelation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+
+
+class RecallScore {
+
+    private HashMap<String, BioCDocument> _docHashMap;
+
+    public  double CalculateScore(BioCCollection trainingSet, BioCCollection predictedSet){
+        //Preprocess , build training set hash
+        int actualTotalRel = GetTotalRelation(trainingSet);
+        int predCorrectRel = 0;
+        HashMap<String, BioCDocument> trainDocHashMap =  buildTrainingSetHash(trainingSet);
+
+        for (BioCDocument predDoc: predictedSet.getDocuments()) {
+            BioCDocument trainingDoc = trainDocHashMap.get(predDoc.getID());
+
+            for (BioCRelation predRelation : predDoc.getRelations()){
+                Optional<String> predPpiRel = predRelation.getInfon("relation");
+                //If not ppim relation ignore relation
+                if(! (predPpiRel.isPresent() && predPpiRel.get().equals("PPIm"))) continue;
+
+               String Gene1 = predRelation.getInfon("Gene1").get();
+               String Gene2 =  predRelation.getInfon("Gene1").get();
+
+               if (ExistsInTraining(trainingDoc, Gene1, Gene2)){
+                   predCorrectRel++;
+               }
+               actualTotalRel++;
+
+
+            }
+        }
+        return  (double)predCorrectRel/(double)actualTotalRel;
+    }
+
+    private int GetTotalRelation(BioCCollection trainingSet) {
+        int total =0;
+        for (BioCDocument doc:trainingSet.getDocuments()   ) {
+            for (BioCRelation relation: doc.getRelations()) {
+
+                Optional<String> ppiRel = relation.getInfon("relation");
+                //If not ppim relation ignore relation
+                if (!(ppiRel.isPresent() && ppiRel.get().equals("PPIm"))) continue;
+
+                 total++;
+
+            }
+
+        }
+        return  total;
+    }
+
+    private boolean ExistsInTraining(BioCDocument trainingDoc, String predGeneRelGene1, String predGeneRelGene2) {
+        for (BioCRelation relation: trainingDoc.getRelations()) {
+
+            Optional<String> ppiRel = relation.getInfon("relation");
+            //If not ppim relation ignore relation
+            if (!(ppiRel.isPresent() && ppiRel.get().equals("PPIm"))) continue;
+
+            String trainGene1 = relation.getInfon("Gene1").get();
+            String trainGene2 =  relation.getInfon("Gene1").get();
+
+            //Use hashmap to check for undirected relationship between 2 genes
+            HashSet<String> trainGenesRelHash = new HashSet<>();
+            trainGenesRelHash.add(trainGene1);
+            trainGenesRelHash.add(trainGene2);
+
+            if (trainGenesRelHash.contains(predGeneRelGene1) && trainGenesRelHash.contains(predGeneRelGene2))
+                return  true;
+
+
+        }
+
+        return false;
+    }
+
+    private HashMap<String, BioCDocument>  buildTrainingSetHash(BioCCollection trainingSet) {
+        HashMap<String, BioCDocument> docHashMap = new HashMap<>();
+        for (BioCDocument doc: trainingSet.getDocuments()   ) {
+            docHashMap.put(doc.getID(), doc);
+
+        }
+        return  docHashMap;
+    }
+
+
+}
---
+++ p2pextraction/src/test/java/ae/nlp/biocreative/RecallScoreTest.java
@@ -0,0 +1,57 @@
+package ae.nlp.biocreative;
+
+import ae.nlp.biocreative.helpers.ConfigHelper;
+import com.pengyifan.bioc.BioCCollection;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by aparnaelangovan on 5/08/2017.
+ */
+class RecallScoreTest {
+    private RecallScore _sut;
+    private String _testdatadir;
+
+    @BeforeEach
+    void setUp() {
+        _sut = new RecallScore();
+        //Testdata
+        _testdatadir = ConfigHelper.getTestDataDirectory();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+
+    void calculateScore() throws SAXException, XMLStreamException, ParserConfigurationException, IOException {
+        //Arrange
+        File sampletraindatafile = Paths.get(_testdatadir, "relationtrainingdata.xml").toAbsolutePath().toFile();
+        File samplePreddatafile = Paths.get(_testdatadir, "relationPred_relation.xml").toAbsolutePath().toFile();
+
+        BioCCollection predSet =new Parser().getBioCCollection(samplePreddatafile).readCollection();
+        BioCCollection trainingSet=new Parser().getBioCCollection(sampletraindatafile).readCollection();
+        
+        //Act
+        double actual = _sut.CalculateScore(trainingSet, predSet);
+
+        //Assert
+        DecimalFormat numFormat = new DecimalFormat("0.000");
+        assertEquals(numFormat.format(353), numFormat.format(actual));
+    }
+
+}
