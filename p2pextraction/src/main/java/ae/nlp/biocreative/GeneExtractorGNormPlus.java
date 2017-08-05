0561a4ea743569a0f27f916edae34ef0e9898508
elangovana <aparnaelangovan@gmail.com>
2017-08-05 22:01:43 +1000

    Fix typo in test

--- p2pextraction/src/main/java/ae/nlp/biocreative/GeneExtractorGNormPlus.java
+++ p2pextraction/src/main/java/ae/nlp/biocreative/GeneExtractorGNormPlus.java
@@ -1,6 +1,7 @@
 package ae.nlp.biocreative;
 
 import com.pengyifan.bioc.io.BioCCollectionReader;
+import org.apache.commons.lang.NotImplementedException;
 
 import java.io.File;
 
@@ -8,7 +9,9 @@ import java.io.File;
  * Created by aparnaelangovan on 27/07/2017.
  */
 public class GeneExtractorGNormPlus {
+
     public File extract(File file) {
-        return file;
+
+        throw  new NotImplementedException("Please run gnormplus externally..");
     }
 }
