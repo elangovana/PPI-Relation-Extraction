647d88b94836683ba5fe433bb57396437bbcf816
aeg <aeg@amazon.com>
2017-11-28 13:36:50 +1100

    Add AWS lambda S3 handler and move  maven plugin and junit settings into parent pom

--- awslambda/src/main/java/ae/nlp/biocreative/awslambda/S3EventHandler.java
+++ awslambda/src/main/java/ae/nlp/biocreative/awslambda/S3EventHandler.java
@@ -1,10 +1,22 @@
 package ae.nlp.biocreative.awslambda;
+import ae.nlp.biocreative.Pipeline;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 /**
@@ -28,15 +40,36 @@ public class S3EventHandler implements RequestHandler<S3Event, String>
             S3EventNotificationRecord record = input.getRecords().get(0);
 
             // Source File Name
-            String srcFileName = record.getS3().getObject().getKey(); // Name doesn't contain any special characters
+            String objectKey = record.getS3().getObject().getKey();
+            String bucketName = record.getS3().getBucket().getName();
 
-            // Destination File Name
-            String distFileName = srcFileName + ".mp4";
+            // Temp local copy of s3 Object
+            String tmpInFileName = File.createTempFile("s3Input", ".xml").getAbsolutePath();
+            String tmpOutFileName = File.createTempFile("s3Output", ".xml").getAbsolutePath();
 
+            // get reference to S3 client
+            AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 
+            //Write s3 to file
+            FileOutputStream fos = null;
+            try{
+                fos = new FileOutputStream(new File(tmpInFileName));
+                CopyS3ObjectToFile(s3, bucketName, objectKey, fos);
+            }
+            finally {
+                if (fos != null)fos.close();
+
+            }
+
+            //Invoke Relation Extraction
+            Pipeline pipeline = new Pipeline();
+            pipeline.runRelationExtraction(tmpInFileName,null, tmpOutFileName);
+
+            //write output to s3 object
+            s3.putObject(bucketName, Paths.get(objectKey, (new File(tmpOutFileName)).getName() ).toString(), tmpOutFileName);
 
             //LOG
-            logger.log("Job Created: " );
+            logger.log("Run completed " );
         }
         catch (Exception e)
         {
@@ -44,5 +77,35 @@ public class S3EventHandler implements RequestHandler<S3Event, String>
         }
 
         return "OK";
+    }
+
+
+    /**
+     * Copies an s3 object to an output stream
+     * @param s3 AmazonS3
+     * @param bucketName S3 Bucket Name
+     * @param objectKey S3 Object Key
+     * @param outStream Output stream to write the result to
+     * @throws IOException
+     */
+    private void CopyS3ObjectToFile(AmazonS3 s3, String bucketName, String objectKey, OutputStream outStream) throws IOException {
+        //Get s3 Object
+        S3ObjectInputStream s3InStream = null;
+        try{
+            S3Object s3Object = s3.getObject(bucketName, objectKey);
+            s3InStream = s3Object.getObjectContent();
+
+            //write stream to file
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3InStream.read(read_buf)) > 0) {
+                outStream.write(read_buf, 0, read_len);
+            }
+        }
+        finally {
+            if (s3InStream != null)
+                s3InStream.close();
+
+        }
     }
 }
